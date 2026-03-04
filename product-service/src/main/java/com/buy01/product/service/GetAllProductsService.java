f382666d3bf837a1bff72a6847a79f2f1c511b96
abrakingoo <maikoabraham68@gmail.com>
2026-03-04 13:31:35 +0300

    feat(product): add service for retrieving all products

---
+++ product-service/src/main/java/com/buy01/product/service/GetAllProductsService.java
@@ -0,0 +1,24 @@
+package com.buy01.product.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.buy01.product.dto.ProductRequst;
+import com.buy01.product.models.Product;
+import com.buy01.product.repository.ProductsRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class GetAllProductsService {
+    
+    private final ProductsRepository productRepo;
+
+    public Iterable<ProductRequst> getAll() {
+        List<Product> product = productRepo.findAll();
+
+        return product.stream().map(p -> new ProductRequst(p.getId(), p.getName(), p.getDescription(), p.getPrice(), p.getQuantity())).toList();
+    }
+}
