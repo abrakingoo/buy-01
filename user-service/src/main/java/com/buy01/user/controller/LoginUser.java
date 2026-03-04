3c54f3f3ae027b009d1f15e5e6d88872cc9a2c5b
abrakingoo <maikoabraham68@gmail.com>
2026-03-04 10:06:30 +0300

    feat(user): implement login endpoint with credentials validation

--- user-service/src/main/java/com/buy01/user/controller/LoginUser.java
+++ user-service/src/main/java/com/buy01/user/controller/LoginUser.java
@@ -1,15 +1,25 @@
 package com.buy01.user.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.buy01.user.dto.LoginCredentials;
 
 @RestController
 @RequestMapping("/api/auth")
 public class LoginUser {
 
     @GetMapping("/login")
-    public String Login() {
-        return "login user";
+    public String LoginForm() {
+        return "login Form";
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> Login(@RequestBody LoginCredentials loginCredentials) {
+        return ResponseEntity.ok(loginCredentials);
     }
 }
