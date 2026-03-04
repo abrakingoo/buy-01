e676a44f05228a1402e607246e1ea30555b29826
abrakingoo <maikoabraham68@gmail.com>
2026-03-04 13:30:53 +0300

    feat(product): update NewProduct DTO

--- product-service/src/main/java/com/buy01/product/dto/NewProduct.java
+++ product-service/src/main/java/com/buy01/product/dto/NewProduct.java
@@ -1,3 +1,3 @@
 package com.buy01.product.dto;
 
-public record NewProduct(String name, String description, double price) {}
+public record NewProduct(String name, String description, double price, int quantity) {}
