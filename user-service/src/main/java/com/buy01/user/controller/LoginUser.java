590b311ae9e200d480cf03b9c93c60ff52253cf4
abrakingoo <maikoabraham68@gmail.com>
2026-03-04 11:26:55 +0300

    feat(user): enhance login endpoint with response entity handling

--- user-service/src/main/java/com/buy01/user/controller/LoginUser.java
+++ user-service/src/main/java/com/buy01/user/controller/LoginUser.java
@@ -1,6 +1,5 @@
 package com.buy01.user.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,10 +7,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.buy01.user.dto.LoginCredentials;
+import com.buy01.user.dto.UserDetails;
+import com.buy01.user.model.User;
+import com.buy01.user.service.UserLoginService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class LoginUser {
+
+    private final UserLoginService userloginService;
 
     @GetMapping("/login")
     public String LoginForm() {
@@ -19,7 +26,11 @@ public class LoginUser {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> Login(@RequestBody LoginCredentials loginCredentials) {
-        return ResponseEntity.ok(loginCredentials);
+    public UserDetails Login(@RequestBody LoginCredentials loginCredentials) {
+        User user = userloginService.login(loginCredentials);
+        if (user == null) {
+            return null;
+        }
+        return new UserDetails(user.getId(), user.getName(), user.getEmail(), user.getRole());
     }
 }
