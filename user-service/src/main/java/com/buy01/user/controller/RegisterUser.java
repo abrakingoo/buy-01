eef8d00c863953f245a54842c07e17aa7bb50a6d
abrakingoo <maikoabraham68@gmail.com>
2026-03-04 10:06:25 +0300

    feat(user): implement user registration endpoint

--- user-service/src/main/java/com/buy01/user/controller/RegisterUser.java
+++ user-service/src/main/java/com/buy01/user/controller/RegisterUser.java
@@ -1,16 +1,36 @@
 package com.buy01.user.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.buy01.user.dto.UserRegistartionDetails;
+import com.buy01.user.model.User;
+import com.buy01.user.service.UserRegistarionService;
+import com.buy01.user.dto.UserDetails; 
+
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class RegisterUser {
 
+    private final UserRegistarionService userRegistarionService;
+    
     @GetMapping("/register")
     public String registerUser(){
-        return "register user";
+        return "registration form";
+    }
+
+    @PostMapping("/register")
+    public UserDetails register(@RequestBody UserRegistartionDetails userRegistartionDetails){
+        User user = userRegistarionService.saveUser(userRegistartionDetails);
+        System.out.println("User registered: " + user);
+        return new com.buy01.user.dto.UserDetails(user.getId(), user.getName(), user.getEmail(), user.getRole());
     }
     
 }
