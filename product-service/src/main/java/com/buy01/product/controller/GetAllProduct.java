1df41eded57c6d6cb49723b2a4c48737970632b1
abrakingoo <maikoabraham68@gmail.com>
2026-03-04 13:31:00 +0300

    feat(product): update product controllers with service integration

--- product-service/src/main/java/com/buy01/product/controller/GetAllProduct.java
+++ product-service/src/main/java/com/buy01/product/controller/GetAllProduct.java
@@ -5,12 +5,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.buy01.product.dto.ProductRequst;
+import com.buy01.product.service.GetAllProductsService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/products")
+@RequiredArgsConstructor
 public class GetAllProduct {
     
+    private final GetAllProductsService getAllProductsService;
+
     @GetMapping
-    public String getAllProduct(){
-        return "get all product";
+    public Iterable<ProductRequst>  getAllProduct(){
+        return getAllProductsService.getAll();
     }
 }
--- product-service/src/main/java/com/buy01/product/controller/GetProductById.java
+++ product-service/src/main/java/com/buy01/product/controller/GetProductById.java
@@ -1,18 +1,24 @@
 package com.buy01.product.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.buy01.product.dto.NewProduct;
+import com.buy01.product.service.GetProductByIdService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/products")
+@RequiredArgsConstructor
 public class GetProductById {
 
+    private final GetProductByIdService getProduct;
+
     @GetMapping("/{id}")
-    public NewProduct getProductById(@PathVariable Long id){
-        return new NewProduct("name", "description", 23.2);
+    public ResponseEntity<?> getProductById(@PathVariable String id){
+        return getProduct.get(id);
     }
 }
