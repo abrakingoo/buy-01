d45b1728cc61fc91820091e0b67b97528793dec9
abrakingoo <maikoabraham68@gmail.com>
2026-03-04 13:30:37 +0300

    feat(product): implement get product by ID with Optional handling

---
+++ product-service/src/main/java/com/buy01/product/service/GetProductByIdService.java
@@ -0,0 +1,31 @@
+package com.buy01.product.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.buy01.product.dto.ProductRequst;
+import com.buy01.product.repository.ProductsRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class GetProductByIdService {
+
+    private final ProductsRepository productRepo;
+
+    public ResponseEntity<?> get(String id) {
+        var product = productRepo.findById(id);
+        if (product.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(
+            new ProductRequst(
+                product.get().getId(), 
+                product.get().getName(), 
+                product.get().getDescription(), 
+                product.get().getPrice(), 
+                product.get().getQuantity())
+        );
+    }
+}
