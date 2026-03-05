e1bd119d8ef1a011b4449f08085366205b8c6997
abrakingoo <maikoabraham68@gmail.com>
2026-03-05 13:07:20 +0300

    feat(product-detail): add product detail page component

---
+++ frontend/src/app/pages/product-detail/product-detail.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Location } from '@angular/common';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
+
+@Component({
+  selector: 'app-product-detail',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './product-detail.component.html',
+  styleUrl: './product-detail.component.css'
+})
+export class ProductDetailComponent implements OnInit {
+  product: Product | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.productService.getProductById(params['id']).subscribe({
+        next: (product) => {
+          this.product = product;
+        },
+        error: (err) => console.error('Failed to load product', err)
+      });
+    });
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
